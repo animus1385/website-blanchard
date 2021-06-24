2a4dc649d7edba2158175cfb580faa835a8b5b0e
Roman Raspopov <animus1385@gmail.com>
2021-06-24 22:32:40 +0300

    fix homework

--- js/header-buttons.js
+++ js/header-buttons.js
@@ -2,8 +2,9 @@ let link = document.querySelectorAll('.item-bottom');
 let contentHeader = document.querySelector('.header__content');
 
 link.forEach(e => {
-    e.addEventListener('click', function (el) {
-        contentHeader.classList.toggle('header__content--active')
+    e.addEventListener('click', function () {
+        e.classList.toggle('active-icon');
+        contentHeader.classList.toggle('header__content--active');
         e.appendChild(contentHeader)
 
     })
