bc2fed4b5f4a0cae40d1985fbfdeebb6f72461a4
Roman Raspopov <animus1385@gmail.com>
2022-01-30 17:44:36 +0300

    fix homework

--- README.md
+++
@@ -1,2 +0,0 @@
-# website-blanchard
-# website-blanchard
--- js/header/search.js
+++ js/header/search.js
@@ -1,15 +1,15 @@
 export default function search() {
     document.querySelector(".header__button-open").addEventListener("click", function () {
         document.querySelector(".header__search-full").classList.toggle("form__active");
-        this.classList.add("active");
+        document.querySelector('.unity-bottom').classList.toggle('unity-active');
+        this.classList.toggle("active");
     })
 
     document.addEventListener("click", function (e) {
         let target = e.target;
         let form = document.querySelector(".form");
         if (!target.closest(".header__search-block")) {
-            form.classList.remove("header__search-full");
-            form.querySelector(".header__search").value = "";
+           
             document.querySelector(".header__button-open").classList.remove("active")
         }
     })
