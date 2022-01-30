67ca2d72db52249ac4dd9c7886c22dabff191f28
Roman Raspopov <animus1385@gmail.com>
2022-01-31 00:50:28 +0300

    fix homework

--- js/editions/filter.js
+++ js/editions/filter.js
@@ -1,8 +1,9 @@
 export function filter() {
     (() => {
-        const checkBtn = document.querySelector('.js-check-heading');    
+        const checkBtn = document.querySelector('.js-check-heading');
         checkBtn.addEventListener('click', function () {
             this.classList.toggle('is-active');
+            this.children[0].classList.toggle('active-arrow');
         });
     })();
 }
--- js/header/burger.js
+++ js/header/burger.js
@@ -8,7 +8,16 @@ export function burgerMenu() {
         $(".header__menu, .header__list, .header__akk").toggleClass("is-active"),
             $("body").toggleClass("lock");
     });
-    
+    resize()
+    window.addEventListener('resize', resize)
+    function resize() {
+        if (document.body.offsetWidth < 1024) {
+            $('.header__menu').append($(".header__akk"))
+        } else {
+            console.log($('.header__log-in'))
+            $('.header__log-in').before($(".header__akk"))
+        }
+    }
 }
 
 
